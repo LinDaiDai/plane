4a298ecacf1a6caa74c502432b0af951affbf66f
LinDaiDai <244508795@qq.com>
2017-08-06 20:39:35 +0800

    add the mode to plane

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 /**
  * Created by Administrator on 2017/8/2.
  */
-window.onload = function () {
+window.onload = function (ev) {
     let homePage = document.getElementById('homePage');
     let game = document.getElementById('game');
     let p = document.getElementById('p');
     let startBtn = document.querySelector('#startBtn');
+
 
     let plane = document.createElement('img');    //创建飞机
 
@@ -20,36 +21,84 @@ window.onload = function () {
     let score = 0;
 
     let timeEnemy;
-
-
-
-    startBtn.onclick = function () {
-
+    //按回车键开始游戏
+    document.onkeydown = function (e) {
+        let ev = event||ev;
+        if(ev.keyCode===13){
+            start();
+        }
+    }
+    //点击按钮开始游戏
+    startBtn.onclick = function(){
+        start();
+    }
+    //定义开始游戏函数
+    function start() {
         createPlane(game);              //引用创建飞机函数
         // createshield(game);             //引用创建护罩函数
+        plane.style.display = 'block';
+        ball.style.display = 'block';
+        shield.style.display = 'block';
+        homePage.style.cursor = 'none';           //隐藏鼠标
+
+        startBtn.parentNode.style.display = 'none';  //隐藏开始按钮
+        game.style.background = 'url("img/center.jpg")';//改变主页背景颜色
         setTimeout(function stepBall() {
             createBall(homePage);       //引用创建子弹函数
             setTimeout(stepBall, 40);
         }, 0);
-
         setTimeout(function stepEnemy() {
             createEnemy(homePage);      //引用创建敌机函数
             timeEnemy=setTimeout(stepEnemy, 500);
         }, 0);
-
-        document.onmousemove = function (ev) {
-            plane.style.display = 'block';
-            ball.style.display = 'block';
-            shield.style.display = 'block';
-            homePage.style.cursor = 'none';           //隐藏鼠标
-
-            startBtn.parentNode.style.display = 'none';  //隐藏开始按钮
-            game.style.background = 'url("img/center.jpg")';//改变主页背景颜色
-
+            //引用获得模式函数
+        let radio = document.getElementsByName('mode');
+        var modeValue = getMode(radio);
+        console.log(modeValue)
+        if(modeValue==1){
+            document.onmousemove = function (ev) {
+                move(ev,modeValue)
+            };
+        }else if(modeValue==2){
+            document.onkeydown = function(){
+                move(ev,modeValue)
+            };
+        }
+            function  move(ev,value){
             let enemys = document.querySelectorAll('.enemy');   //获取敌机集合
             let balls = document.querySelectorAll('.ball');     //获取子弹集合
             let enemyBalls = document.querySelectorAll('.enemyBall');
-            for(let i=0;i<balls.length;i++){        //对子弹集合遍历
+            var oEvent = event || ev;
+                //判断模式
+                if(value == 2){                         //键盘模式
+                    //获取事件对象
+                    switch (oEvent.keyCode){
+                        case 65:                //左
+                        case 37:
+                            plane.style.left=plane.offsetLeft-40+'px';
+                            break;
+                        case 68:                //右
+                        case 39:
+                            plane.style.left=plane.offsetLeft+40+'px';
+                            break;
+                        case 87:                //上
+                        case 38:
+                            plane.style.top=plane.offsetTop-40+'px';
+                            break;
+                        case 83:                    //下
+                        case 40:
+                            plane.style.top=plane.offsetTop+40+'px';
+                            break;
+                    }
+                }else if(value==1){                 //鼠标模式
+                    var pos = getPos(oEvent);       //引用鼠标位置函数
+                    var posX = pos.x - plane.offsetWidth/2;
+                    var posY = pos.y;
+                    plane.style.left = posX + 'px';    //设置飞机的位置
+                    plane.style.top = posY + 'px';
+                }
+
+                for(let i=0;i<balls.length;i++){        //对子弹集合遍历
                 if(balls[i].offsetTop<=0){          //若子弹超出范围
                     homePage.removeChild(balls[i]);
                 }else{
@@ -70,15 +119,6 @@ window.onload = function () {
                     }
                 }
             }
-
-            var oEvent = event || ev;         //获取事件对象
-            var pos = getPos(oEvent);       //引用鼠标位置函数
-            var posX = pos.x - plane.offsetWidth/2;
-            var posY = pos.y;
-            plane.style.left = posX + 'px';    //设置飞机的位置
-            plane.style.top = posY + 'px';
-            shield.style.left = posX + 'px';    //设置飞机的位置
-            shield.style.top = posY + 'px';
 
             //对飞机超出边界做判断
             var maxLeft = parseInt(470+homePage.offsetWidth-plane.offsetWidth);
@@ -112,13 +152,29 @@ window.onload = function () {
             }
         };
     };
-
+    //获取模式
+    function getMode(radio){
+        var mode;
+        for(var r of radio){
+            if(r.checked==true){
+                var val = r.value;
+                if(val==1){
+                    mode=1;
+                }else if(val==2){
+                    mode=2;
+                }
+            }
+        }
+        return mode;
+    }
 
     //生成飞机
     function createPlane(parent) {
         plane.className = 'plane';
         plane.src = 'img/plane3.png';
         plane.style.display = 'none';
+        plane.style.left=560+'px';
+        plane.style.top=450+'px';
         game.appendChild(plane);
     }
 
@@ -192,7 +248,7 @@ window.onload = function () {
         enemyBall.style.top = enemyBallTop + 'px';
         enemyBall.onload=function(){
             setTimeout(function enemyBallMove(){
-                if(enemyBall.offsetTop>=600){
+                if(enemyBall.offsetTop>=550){
                     homePage.removeChild(enemyBall);
                 }
                 enemyBall.style.top=enemyBall.offsetTop+20+'px';
@@ -201,8 +257,6 @@ window.onload = function () {
         };
         parent.appendChild(enemyBall);
     }
-
-
 
     //定义爆炸条件
 
@@ -215,17 +269,21 @@ window.onload = function () {
     }
 
     //定义游戏结束
+    var count =0;
     function gameover(enemyBalls,plane){
         for(let n=0;n<enemyBalls.length;n++){
                 if((enemyBalls[n].offsetTop+enemyBalls[n].offsetHeight>=plane.offsetTop&&enemyBalls[n].offsetTop<=plane.offsetTop+plane.offsetHeight)&&((enemyBalls[n].offsetLeft>=parseInt(plane.offsetLeft)+10)&&enemyBalls[n].offsetLeft<=(plane.offsetLeft+plane.offsetWidth-10))){
+                    count++;
+                }
+                if(count==1){
+                    plane.src='img/plane4.png';
+                    return false
+                }else if(count==2){
                     return true;
                 }
         }
         return false;
     }
-
-
-
     //定义函数  获取鼠标移动时的坐标信息；
     function getPos(ev) {
         var e = ev || window.event;
@@ -239,6 +297,6 @@ window.onload = function () {
 
     //定义一个求范围的随机数的函数
     function randomInt(from, to) {
-        return parseInt(((Math.random() * (to + 1 - from)) + from));
+        return parseInt((Math.random() * (to + 1 - from)) + from);
     }
 };
